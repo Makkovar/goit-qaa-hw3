c1479b1c9e3532fe9ba594f0f2ddf5ee9477bcd9
Maciek P <maciej.paszkowski93@gmail.com>
2024-05-24 19:38:39 +0200

    wykonane zadanie

---
+++ arrays.js
@@ -0,0 +1,9 @@
+
+const fruits = ["Jablko", "Banan", "Winogrona", "Arbuz"];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13];
+
+for (const number of numbers) {
+    if (number>5)
+    console.log(number);
+}
